c27ce16d82e375c127161a9586569fa529eb30be
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-02-10 14:16:17 +0100

    Card

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include "head.php";
+?>
+
+<div class="post row">
+    <div class="card col-xl-3">
+        <img src="img/park.jpg" class="card-img-top " alt="">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+            <a href="#" class="btn">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card col-xl-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card col-xl-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+</div>
+
+<?php
+include "foot.php"
+?>
