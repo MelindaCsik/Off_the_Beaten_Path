f01d9123a70d19da20a5d2767e4713473cc1c1fe
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-02-17 13:33:49 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,26 +1,29 @@
 <?php
-include "head.php";
+include "head.inc.php";
 ?>
 
-<div class="container mt-5">
-        <h2 class="mb-4">Regisztráció</h2>
+<div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="p-4 rounded shadow bg-white form-container registrationDiv" >
+        <h2 class="text-center mb-4">Regisztráció</h2>
         <form>
-        <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="username" placeholder="Felhasználónév">
-            <label for="username">Felhasználónév</label>
-        </div>
-        <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="email" placeholder="Email cím">
-            <label for="email">Email cím</label>
-        </div>
-        <div class="form-floating">
-            <input type="password" class="form-control" id="password" placeholder="Jelszó">
-            <label for="password">Jelszó</label>
-        </div>
-            <button type="submit" class="btn">Regisztráció</button>
+            <div class="form-floating mb-3 input-width">
+                <input type="text" class="form-control" id="username" placeholder="Felhasználónév">
+                <label for="username">Felhasználónév</label>
+            </div>
+            <div class="form-floating mb-3 input-width">
+                <input type="email" class="form-control" id="email" placeholder="Email cím">
+                <label for="email">Email cím</label>
+            </div>
+            <div class="form-floating mb-3 input-width">
+                <input type="password" class="form-control" id="password" placeholder="Jelszó">
+                <label for="password">Jelszó</label>
+            </div>
+            <button type="submit" class="btn w-100 gomb">Regisztráció</button>
         </form>
+    </div>
 </div>
 
+
 <?php
-include "foot.php";
+include "foot.inc.php";
 ?>
