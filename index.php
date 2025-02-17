a5a47f5fa116af7ebdbacc57d8a09d9b14a68f52
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-02-17 14:27:39 +0100

    Keresőmező

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 include "./common/head.inc.php";
 ?>
 
+<form class="row g-3 search">
+  <div class="col-auto">
+    <input type="search" class="form-control" id="inputSearch" placeholder="Keresés...">
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+        </svg>
+    </button>
+  </div>
+</form>
+
 <div class="container">
 <div class="row posts">
     <div class="card col-xl-3 post">
