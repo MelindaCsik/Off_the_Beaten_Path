94d5c05ee7b215b5a3038e499ad6b3701f013225
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-02-17 09:30:23 +0100

    Bejelentkezési oldal létrehozása

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include "head.php";
+?>
+
+<div class="container mt-5">
+        <h2 class="mb-4">Bejeletkezés</h2>
+        <form>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="username" placeholder="Felhasználónév">
+            <label for="username">Felhasználónév</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="password" placeholder="Jelszó">
+            <label for="password">Jelszó</label>
+        </div>
+            <button type="submit" class="btn">Regisztráció</button>
+        </form>
+</div>
+
+<?php
+include "foot.php";
+?>
