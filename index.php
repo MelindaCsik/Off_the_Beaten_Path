9c1e01f1f674d5fa3cb733efe3099e20ceecb6c6
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-02-17 09:30:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include "head.php";
 ?>
 
-<div class="post row">
-    <div class="card col-xl-3">
+<div class="container">
+<div class="posts row">
+    <div class="card col-xl-3 post">
         <img src="img/park.jpg" class="card-img-top " alt="">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
@@ -30,8 +31,26 @@ include "head.php";
         </div>
     </div>
 
+    <div class="card col-xl-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card col-xl-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
 </div>
 
 <?php
-include "foot.php"
+include "foot.php";
 ?>
