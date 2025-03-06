be0e4250c392b350b6f540ec1d7f5354efac181f
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-03-06 11:25:43 +0100

    Profil oldal létre hozzása

---
+++ profile.php
@@ -0,0 +1,28 @@
+<?php
+include "./common/head.inc.php";
+?>
+
+
+<div class="container">
+    <h1>Profil adatok</h1>
+    <div class="userData">
+
+    </div>
+
+    <h2>Saját bejegyzések</h2>
+    <div class="line"></div>
+    <div class="row posts">
+        <div class="card col-lg-3 post">
+            <img src="img/park.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content...</p>
+                <a href="#" class="btn">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include "./common/foot.inc.php";
+?>
