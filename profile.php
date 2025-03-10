46b9e9e08466bd4791db36480826a3d512aba803
MelindaCsik <83371046+MelindaCsik@users.noreply.github.com>
2025-03-10 13:56:20 +0100

    Profil adatok

--- profile.php
+++ profile.php
@@ -6,7 +6,13 @@ include "./common/head.inc.php";
 <div class="container">
     <h1>Profil adatok</h1>
     <div class="userData">
+        <p>Felhasználónév</p>
+        <p class="dataField">XY</p>
 
+        <p>Email cím</p>
+        <p class="dataField">xy@email.com</p>
+
+        <button type="submit" class="btn w-100 gomb">Módosítás</button>
     </div>
 
     <h2>Saját bejegyzések</h2>
